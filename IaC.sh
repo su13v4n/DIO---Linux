81543ac637158c5760cf767ae11553897cc49183
su13v4n <sulevanspz@gmail.com>
2022-08-10 11:09:48 +0000

    adicionado expiração da senha ao fazer login

--- IaC.sh
+++ IaC.sh
@@ -16,19 +16,19 @@ groupadd GRP_SEC
 echo "Criando usuários..."
 
 echo "Group ADM..."
-useradd carlos -c "Carlos Alberto" -m -s/bin/bash -p $(openssl passwd senha123) -G GRP_ADM
-useradd ana -c "Ana Cristina spoll" -m -s/bin/bash -p $(openssl passwd senha123) -G GRP_ADM
-useradd Andressa -c "Andressa Sant'anna" -m -s/bin/bash -p $(openssl passwd senha123) -G GRP_ADM
+useradd carlos -c "Carlos Alberto" -m -s/bin/bash -p $(openssl passwd senha123) -e -G GRP_ADM
+useradd ana -c "Ana Cristina spoll" -m -s/bin/bash -p $(openssl passwd senha123) -e -G GRP_ADM
+useradd Andressa -c "Andressa Sant'anna" -m -s/bin/bash -p $(openssl passwd senha123) -e -G GRP_ADM
 
 echo "Group VEN..."
-useradd elvis -c "Elvis Eduardo" -m -s/bin/bash -p $(openssl passwd senha123) -G GRP_VEN
-useradd francisco -c "Francisco Xavier" -m -s/bin/bash -p $(openssl passwd senha123) -G GRP_VEN
-useradd santos -c "Santos Assis" -m -s/bin/bash -p $(openssl passwd senha123) -G GRP_VEN
+useradd elvis -c "Elvis Eduardo" -m -s/bin/bash -p $(openssl passwd senha123) -e -G GRP_VEN
+useradd francisco -c "Francisco Xavier" -m -s/bin/bash -p $(openssl passwd senha123) -e -G GRP_VEN
+useradd santos -c "Santos Assis" -m -s/bin/bash -p $(openssl passwd senha123) -e -G GRP_VEN
 
 echo "Group SEC..."
-useradd carolina -c "Carolinha Oliveira" -m -s/bin/bash -p $(openssl passwd senha123) -G GRP_SEC
-useradd zelia -c "Zelia Alcantara" -m -s/bin/bash -p $(openssl passwd senha123) -G GRP_SEC
-useradd joaquim -c "Joaquim Ez" -m -s/bin/bash -p $(openssl passwd senha123) -G GRP_SEC
+useradd carolina -c "Carolinha Oliveira" -m -s/bin/bash -p $(openssl passwd senha123) -e -G GRP_SEC
+useradd zelia -c "Zelia Alcantara" -m -s/bin/bash -p $(openssl passwd senha123) -e -G GRP_SEC
+useradd joaquim -c "Joaquim Ez" -m -s/bin/bash -p $(openssl passwd senha123) -e -G GRP_SEC
 
 echo "Especificando as permissões..."
 
